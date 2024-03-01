6fefe41a2e86f8427555df4e9b7e9f714eb204f5
Stephanie <yesstephanie1@gmail.com>
2024-02-29 22:41:48 -0800

    test successful run

--- notely_test.go
+++ notely_test.go
@@ -4,37 +4,17 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-// func Test_addParseTimeParam(t *testing.T) {
-// 	testCases := []struct {
-// 		name     string
-// 		input    string
-// 		expected string
-// 		err      error
-// 	}{
-// 		{"ValidURL", "mysql://user:pass@host/db", "mysql://user:pass@host/db?parseTime=true", nil},
-// 		{"InvalidURL", "not-a-url", "", nil},
-// 		{"MissingScheme", "user:pass@host/db", "http://user:pass@host/db?parseTime=true", nil},
-// 	}
-
-// 	for _, tc := range testCases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			actual, err := addParseTimeParam(tc.input)
-// 			assert.Equal(t, tc.expected, actual)
-// 			assert.Equal(t, tc.err, err)
-// 		})
-// 	}
-// }
-
-func Test_handlerReadiness(t *testing.T) {
-	req := httptest.NewRequest("GET", "/v1/healthz", nil)
+func TestHandlerReadiness(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/v1/healthz", nil)
 	rr := httptest.NewRecorder()
 
-	handlerReadiness(rr, req)
+	handler := http.HandlerFunc(handlerReadiness)
+	handler.ServeHTTP(rr, req)
 
-	assert.Equal(t, http.StatusOK, rr.Code)
-	assert.Equal(t, "{\"status\":\"ok\"}", rr.Body.String())
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handlerReadiness returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
 }
